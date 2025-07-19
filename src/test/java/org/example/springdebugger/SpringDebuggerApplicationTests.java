d29e283755912e29d10d58afa756d3a09b9abb86
Anshuman Mishra <49508586+anshuman35@users.noreply.github.com>
2025-07-19 10:02:45 +0530

    Update SpringDebuggerApplicationTests.java

--- src/test/java/org/example/springdebugger/SpringDebuggerApplicationTests.java
+++ src/test/java/org/example/springdebugger/SpringDebuggerApplicationTests.java
@@ -3,13 +3,16 @@ package org.example.springdebugger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-
+import org.springframework.context.annotation;
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class SpringDebuggerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    public void contextLoadsOnInit(){
+
     }
 
 }
